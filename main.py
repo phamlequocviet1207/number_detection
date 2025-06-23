6e473181dfa4fa9a6f60e553b6e6b0c6351d8db1
phamlequocviet1207 <108972420+phamlequocviet1207@users.noreply.github.com>
2025-06-22 22:49:36 -0400

    Adding data

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 import numpy as np
+import cv2
+import os
+
+################
+path = 'myData'
+sizeImg = (32,32)
+################
+
+images = []
+classNo = []
+myList = os.listdir(path)
+noOfClasses = len(myList)
+
+print("Importing class .........")
+
+
+for x in range (noOfClasses):
+    myPiclist = os.listdir(path+"/"+str(x))
+    for y in myPiclist:
+        curImg = cv2.imread(path+"/"+str(x)+"/"+y)
+        curImg = cv2.resize(curImg, sizeImg)
+        images.append(curImg)
+        classNo.append(x)
+    print(x,end=" ")
+ 
+print()
+
+images = np.array(images)
+classNo = np.array(classNo)
+
+print(images.shape)
+print(classNo.shape)
+    
